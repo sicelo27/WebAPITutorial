67782a83159aeed1a3068c5cd6e8c044c61f344d
sicelo27 <ssitsha@wcyber.net>
2024-05-13 12:04:59 +0200

    add shirt repository to fetch data

--- WebAPITutorial/Controllers/ShirtsController.cs
+++ WebAPITutorial/Controllers/ShirtsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebAPITutorial.Models;
+using WebAPITutorial.Models.Repositories;
 
 namespace WebAPITutorial.Controllers
 {
@@ -8,16 +9,6 @@ namespace WebAPITutorial.Controllers
     [Route("api/[controller]")]
     public class ShirtsController : ControllerBase
     {
-        private List<Shirt> shirts = new List<Shirt>()
-        {
-            new Shirt {ShirtId = 1, Brand = "My Brand", Color = "Blue", Gender = "Men", Price = 30, Size = 10},
-            new Shirt {ShirtId = 2, Brand = "My Brand", Color = "Black", Gender = "Men", Price = 24, Size = 8},
-            new Shirt {ShirtId = 3, Brand = "Your Brand", Color = "Green", Gender = "Women", Price = 32, Size = 6},
-            new Shirt {ShirtId = 4, Brand = "Her Brand", Color = "Pink", Gender = "Women", Price = 10, Size = 10},
-
-
-
-        };
         [HttpGet]
         public string GetShirts()
         {
@@ -30,7 +21,7 @@ namespace WebAPITutorial.Controllers
             if (id < 0)
                 return BadRequest();
 
-            var shirt = shirts.FirstOrDefault(x =>  x.ShirtId == id);
+            var shirt = ShirtRepository.GetShirtById(id);
             if (shirt == null)
                 return NotFound();
 
