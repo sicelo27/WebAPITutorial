879b609efac5b3f332346160aa82724e469e7ee1
sicelo27 <ssitsha@wcyber.net>
2024-05-14 14:33:20 +0200

    add swaggerUI to application

--- WebAPITutorial/Program.cs
+++ WebAPITutorial/Program.cs
@@ -7,6 +7,8 @@ namespace WebAPITutorial
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             builder.Services.AddControllers();
 
             var app = builder.Build();
@@ -20,6 +22,14 @@ namespace WebAPITutorial
            // app.UseAuthorization();
 
             app.MapControllers();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+                options.DocumentTitle = "Shirts Endpoints";
+            });
 
             app.Run();
         }
